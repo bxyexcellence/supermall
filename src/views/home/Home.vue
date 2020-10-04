c0552b77058df79805546aea3f893c2d8d9f63d4
bxyexcellence <1985088145@qq.com>
2020-10-04 17:33:00 +0800

    '首页展示轮播图'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,35 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners = "banner"></home-swiper>
   </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar'
+import NavBar from 'components/common/navbar/NavBar';
+import HomeSwiper from './childCompents/homeSwiper'
+import { getHomeMultidata } from "network/home";
 export default {
   name:'Home',
   components: {
-    NavBar
+    NavBar,
+    HomeSwiper
+  },
+  data(){
+    return {
+      //result:null,
+      banner:[],
+      recommends:[]
+    }
+  },
+  created () {
+    getHomeMultidata().then(res => {
+        console.log(res);
+        //this.result = res;
+        this.banner= res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      }
+    )
   }
 }
 </script>
