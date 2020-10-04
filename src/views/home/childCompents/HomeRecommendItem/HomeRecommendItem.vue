8613eac383652727abb53cdb4e8dfd34650b8a33
bxyexcellence <1985088145@qq.com>
2020-10-04 21:39:15 +0800

    '封装首页推荐组件'

---
+++ src/views/home/childCompents/HomeRecommendItem/HomeRecommendItem.vue
@@ -0,0 +1,44 @@
+<template>
+     <div class="recommend-item">
+      <a :href="item.link">
+      <img :src="item.image" alt="">
+      <div>{{item.title}}</div>
+      </a>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommendItem',
+  props:{
+    item:{
+      type:Object,
+      default(){
+        return {}
+      }
+    }
+  },
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+.recommend-item{
+  flex: 1;
+}
+.recommend-item img{
+  /* height: 65px;
+  width: 65px; */
+  width: auto;
+  height: auto;
+  max-width: 85%;
+  margin-bottom: 10px;
+}
+</style>
---
+++ src/views/home/childCompents/HomeRecommendView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="recommend">
+   <home-recommend-item v-for="(item,index) in recommends" :key="index" :item = "item"></home-recommend-item>
+  </div>
+</template>
+
+<script>
+import HomeRecommendItem from './HomeRecommendItem/HomeRecommendItem'
+export default {
+  name: 'HomeRecommendView',
+  props:{
+    recommends:{
+      type:Array,
+      default(){
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    HomeRecommendItem
+  }
+}
+</script>
+
+<style scoped>
+.recommend{
+  display: flex;
+  text-align: center;
+  font-size: 12px;
+  width: 100%;
+  padding: 10px 0 30px;
+  border-bottom: 8px solid #eee;
+}
+
+</style>
