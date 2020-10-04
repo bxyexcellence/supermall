d8cb1fc19ca2b76a5588ba090ef6de354a23474d
bxyexcellence <1985088145@qq.com>
2020-10-04 17:29:28 +0800

    '轮播图展示'

---
+++ src/views/home/childCompents/homeSwiper.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+     <Swiper>
+      <swiper-item v-for="(item,index) in banners" :key="index">
+        <a :href="item.link">
+          <img :src="item.image" />
+        </a>
+      </swiper-item>
+    </Swiper>
+  </div>
+</template>
+
+<script>
+import { Swiper,SwiperItem } from "components/common/swiper/index.js";
+export default {
+  name: 'homeSwiper',
+  props:{
+    banners:{
+      type:Array,
+      default(){
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    Swiper,
+    SwiperItem
+  }
+}
+</script>
+
+<style scoped >
+</style>
