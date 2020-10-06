16a84dc4cd60258b532d410b5d98b01bfad4ed63
bxyexcellence <1985088145@qq.com>
2020-10-07 00:01:00 +0800

    '封装scroll组件'

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from '@better-scroll/core';
+export default {
+  name: 'Scroll',
+  data(){
+    return {
+      //scroll:null
+    }
+  },
+  components:{
+
+  },
+  created () {
+    
+  },
+  mounted () {
+    
+    setTimeout(()=>{
+      //this.scroll.refresh();
+      this.init()
+    },500)
+    
+  },
+  beforeDestroy() {
+    this.scroll.destroy()
+  },
+  methods: {
+    init(){
+      this.scroll = new BScroll(this.$refs.wrapper,{
+        probeType: 3,
+        click: true,
+        scrollY: true
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* .wrapper{
+  height: 600px;
+  overflow: hidden;
+} */
+</style>
