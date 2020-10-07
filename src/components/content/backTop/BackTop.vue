e604f69c42819def51f4d1fff596cc162577d874
bxyexcellence <1985088145@qq.com>
2020-10-07 17:13:32 +0800

    '封装backTop组件'

---
+++ src/components/content/backTop/BackTop.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="back-top">
+    <img src="~assets/img/common/top.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BackTop',
+  data(){
+    return {
+
+    }
+  },
+  components:{
+
+  }
+}
+</script>
+
+<style scoped>
+.back-top{
+  position: fixed;
+  bottom: 50px;
+  z-index: 4;
+  right: 5px;
+}
+.back-top img{
+  width: auto;
+  height: auto;
+  max-width: 50px;
+}
+</style>
