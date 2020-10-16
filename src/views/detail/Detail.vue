f2346c353a34a3b60174bf695027a00f69ea5d0e
bxyexcellence <1985088145@qq.com>
2020-10-16 23:10:06 +0800

    '展示商品信息，商家，商品详细参数'

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,36 +1,77 @@
 <template>
   <div id="detail">
     <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :topImages="topImages"></detail-swiper>
+    <Scroll class="content" ref="scroll">
+      <detail-swiper :topImages ="topImages"></detail-swiper>
+      <detail-base-info :goods ="goods"></detail-base-info>
+      <detail-shop-info :shop ="shop"></detail-shop-info>
+      <detail-goods-info :detailInfo ="detailInfo" @imageLoad="imageLoad"></detail-goods-info>
+      <detail-param-info :paramInfo="paramInfo"></detail-param-info>
+    </Scroll>
   </div>
 </template>
 
 <script>
 import detailNavBar from './childComps/detailNavBar';
-import detailSwiper from './childComps/detailSwiper'
+import detailSwiper from './childComps/detailSwiper';
+import detailBaseInfo from './childComps/detailBaseInfo'
+import detailShopInfo from './childComps/detailShopInfo'
+import detailGoodsInfo from './childComps/detailGoodsInfo'
+import detailParamInfo from './childComps/detailParamInfo'
 
-import { getDetail } from '../../network/detail'
+import Scroll from 'components/common/scroll/Scroll'
+
+import { getDetail,Goods,Shop, GoodsParam } from 'network/detail'
 export default {
   name: '',
   data(){
     return {
       iid:null,
-      topImages:[]
+      topImages:[],
+      goods:{},
+      shop:{},
+      detailInfo:{},
+      paramInfo:{}
     }
   },
   components:{
    detailNavBar,
-   detailSwiper
+   detailSwiper,
+   detailBaseInfo,
+   detailShopInfo,
+   Scroll,
+   detailGoodsInfo,
+   detailParamInfo
   },
   created () {
     this.iid = this.$route.params.iid
     getDetail(this.iid).then(res=>{
       console.log(res);
-      this.topImages = res.result.itemInfo.topImages 
+      const data = res.result;
+      this.topImages = data.itemInfo.topImages;
+      this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services) 
+      this.shop = new Shop(data.shopInfo)
+      this.detailInfo = data.detailInfo
+      this.paramInfo = new GoodsParam(data.itemParams.info,data.itemParams.rule)
     })
+  },
+  methods: {
+    imageLoad(){
+      this.$refs.scroll.scroll.refresh()
+    }
   }
 }
 </script>
 
 <style scoped>
+#detail{
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+  height: 100vh;
+}
+.content{
+  height: calc(100% - 44px);
+  overflow: hidden;
+}
 </style>
